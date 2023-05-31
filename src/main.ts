a76ef990b5de8a7135cbc6697febf8ef93ff04a0
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-05-30 23:49:32 -0300

    atualizações de Cors para deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Altere para o domínio correto da sua aplicação Vue.js
+    origin: 'https://estagio-legal-front.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
